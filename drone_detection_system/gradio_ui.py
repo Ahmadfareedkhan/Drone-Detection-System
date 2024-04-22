cffa7c362d56a3aac4503f1c0e1f3f5589813e27
Ahmadfareedkhan <ahmadkhanfareed388@gmail.com>
2024-04-22 17:55:01 +0500

    Gradio Interface

---
+++ drone_detection_system/gradio_ui.py
@@ -0,0 +1,35 @@
+import gradio as gr
+from ultralytics import YOLO
+import cv2
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+
+# Load your trained model
+model = YOLO(r"C:\Users\HP\.vscode\drone_detection\Drone-Detection-System\drone_detection_system\best.pt")
+
+def predict_image(image):
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    results = model.track(image, conf=0.3, iou=0.5)
+    output_image = results.render()[0]
+    return cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB)
+
+def predict_video(video):
+    return model.track(video, conf=0.3, iou=0.5, show=True)
+
+with gr.Blocks() as demo:
+    gr.Markdown("### Drone Detection System")
+    with gr.Tab("Introduction"):
+        gr.Markdown("Welcome to the Drone Detection System.")
+    with gr.Tab("Upload Image"):
+        image_input = gr.Image()
+        image_output = gr.Image()
+        image_input.change(fn=predict_image, inputs=image_input, outputs=image_output)
+    with gr.Tab("Upload Video"):
+        video_input = gr.Video()
+        video_output = gr.Video()
+        video_input.change(fn=predict_video, inputs=video_input, outputs=video_output)
+    with gr.Tab("Live"):
+        gr.Markdown("Live detection will be implemented soon.")
+
+demo.launch()
